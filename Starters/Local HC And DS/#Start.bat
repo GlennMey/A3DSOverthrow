79db170510a6d8db6cdb83b3c688abf613139b26
GlennMey <32812151+GlennMey@users.noreply.github.com>
2017-11-09 14:34:33 +0100

    Removed affinity as they are not needed. Removed realtime priority as they are causing crashes and freezes.

--- Starters/Local HC And DS/#Start.bat
+++
@@ -1,2 +0,0 @@
-cd ..\..
-start /AFFINITY 1 /REALTIME "ArmA3DS" .\arma3server_x64.exe "-mod=@CBA_A3;@ace;@Overthrow" -config=Overthrow\Server.cfg -cfg=Overthrow\basic.cfg -profiles=Overthrow -name=Overthrow -malloc=tbb4malloc_bi_x64 -hugepages -noLogs -autoInit -serverMod=
