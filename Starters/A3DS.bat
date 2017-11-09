a4d7504650bcc7c0fecbfd23ebe843bac0087999
GlennMey <32812151+GlennMey@users.noreply.github.com>
2017-11-09 14:38:15 +0100

    Removed Realtime
    As they were causing crashes and other fun experiences

--- Starters/A3DS.bat
+++ Starters/A3DS.bat
@@ -1,2 +1,2 @@
 cd ..\.
-start /REALTIME "ArmA3DS" .\arma3server_x64.exe "-mod=@CBA_A3;@ace;@Overthrow" -config=Overthrow\Server.cfg -cfg=Overthrow\basic.cfg -profiles=Overthrow -name=Overthrow -malloc=tbb4malloc_bi_x64 -hugepages -noLogs -autoInit -serverMod=
+start "ArmA3DS" .\arma3server_x64.exe "-mod=@CBA_A3;@ace;@Overthrow" -config=Overthrow\Server.cfg -cfg=Overthrow\basic.cfg -profiles=Overthrow -name=Overthrow -malloc=tbb4malloc_bi_x64 -hugepages -noLogs -autoInit -serverMod=
